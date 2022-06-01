550014102751f4ce45ca6338e96bcd0db1b16b28
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-01 14:48:58 -0300

    String, números, operações aritméticas

--- hello.dart
+++ hello.dart
@@ -1,3 +1,11 @@
+// Todo arquivo em Dart precisa da extensão ".dart"
 main() {
+  // String
 	print("Hello World");
+
+  // Printando um número
+  print(5);
+
+  // Multiplicação - (Operação Aritmética)
+  print(5*2);
 }
