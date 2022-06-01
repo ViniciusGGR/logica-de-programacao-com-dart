d8e3b5f6f1862f94fdc4c07e63fdd8d0769490f0
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-01 19:59:49 -0300

    concatenação

---
+++ concatenacao.dart
@@ -0,0 +1,11 @@
+main() {
+  // Para fazer uma concatenação de texto, é preciso utilizar o +
+  var text = "Hello " + "World";
+  print(text);
+
+  // Concatenação de texto com variáveis
+  var name = "Vinícius ";
+  var surname = "Gabriel";
+  var fullName = name + surname;
+  print(fullName);
+}
