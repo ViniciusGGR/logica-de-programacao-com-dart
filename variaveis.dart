a28486918a6a8bd6959de6dae26000dbc2b90b57
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-01 15:27:17 -0300

    var

---
+++ variaveis.dart
@@ -0,0 +1,17 @@
+main() {
+  // Criando uma variável
+    // Dica: Nomes de variáveis não podem conter espaços/acentos...
+  var name = "Vinícius Gabriel";
+
+  // Atribuindo outro valor para a variável name:
+  name = "Vinícius Gabriel Gomes Rodrigues";
+
+  print(name);
+
+  var age = 20;
+
+  // Atribuindo outro valor para a variável age:
+  age = 21;
+
+  print(age);
+}
