3d6bf9a4bfcd9a8d2027a7472883e7a5accd42eb
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-01 21:05:37 -0300

    Começando exercicio

---
+++ exercicio.dart
@@ -0,0 +1,16 @@
+main() {
+  // Verificar se uma pessoa é maior de idade (18)
+
+  // Perguntar a idade da pessoa:
+  // Se a idade for maior ou igual a 18, a pessoa é considerada maior de idade.
+  // Se a idade for menor que 18, a pessoa é considerada menor de idade.
+
+  var age = 20;
+
+  if (age >= 18) {
+    print("Você é maior de idade");
+  } else {
+    print("Você é menor de idade");
+  }
+  
+}
