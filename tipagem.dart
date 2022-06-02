fbbf614c79b0417295a39b95582ff82bff09066b
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-02 20:06:05 -0300

    Tipagem

---
+++ tipagem.dart
@@ -0,0 +1,15 @@
+main() {
+  
+  var text = "Hello World";
+  String text1 = "Hello World"; // Variável Tipada.
+  
+  var num1 = 4;
+  int num3 = 4; // Variável Tipada.
+
+  var num2 = 6.5;
+  double num4 = 6.5; // Variável Tipada.
+
+  var test = true;
+  bool test2 = true; // Variável Tipada.
+  
+}
