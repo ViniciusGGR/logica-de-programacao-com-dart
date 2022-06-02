3710253e64fccc4736c8eed7d3d07119d2d7e7a9
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-02 20:35:42 -0300

    Loop for

---
+++ loop.dart
@@ -0,0 +1,10 @@
+main() {
+  
+  // int x = 0 | Variável do tipo inteiro que recebe valor 0.
+  // x <= 10 | Condição - Enquanto x for menor ou igual a 10 o for segue rodando.
+  // x++ | Acrescenta 1 ao valor x da variável.
+    // x++ | x = x + 1.
+  for (int x = 0; x <= 10; x++) { // Esse loop vai correr 10 vezes.
+    print("Número $x");
+  }
+}
