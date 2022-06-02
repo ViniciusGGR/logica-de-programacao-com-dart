605b45fe3580cc8da21d95c60324847442e62e6f
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-01 21:59:30 -0300

    Exercicio idade

--- exercicio.dart
+++ exercicio.dart
@@ -4,14 +4,20 @@ void main() {
   // Verificar se uma pessoa é maior de idade (18)
 
   // Perguntar a idade da pessoa:
-  // Se a idade for maior ou igual a 18, a pessoa é considerada maior de idade.
-  // Se a idade for menor que 18, a pessoa é considerada menor de idade.
+  // Se a idade for maior ou igual a 18
+    // Pessoa é considerada maior de idade.
+  // Se a idade for menor que 18
+    // Pessoa é considerada menor de idade.
 
   print("Qual é a sua idade:");
 
-  String? ageInput = stdin.readLineSync();
-  if (ageInput != null) {
-    int age = int.parse(ageInput);
+  // String? - Cria uma variável com valor nulo.
+  String? ageInput = stdin.readLineSync(); // Retorna uma String.
+  // O método readLineSync() do stdin permite pegar uma String digitada no console.
+
+  if (ageInput != null) { // Verifica se a variável recebeu um valor, não estando mais com o valor nulo.
+
+    int age = int.parse(ageInput); // Faz a conversão da variável ageInput de String para um int/inteiro.
 
     if (age >= 18) {
       print("Você é maior de idade");
