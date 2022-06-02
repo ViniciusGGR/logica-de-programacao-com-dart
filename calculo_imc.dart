6457c4304bcf11afbc818cead60da391c7d40c42
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-02 12:20:21 -0300

    Melhorando o exercicio de cálculo IMC

--- calculo_imc.dart
+++ calculo_imc.dart
@@ -1,28 +1,31 @@
 import 'dart:io';
 
 main() {
+  stdout.write("Qual é o seu nome: ");
+  String name = (stdin.readLineSync()!);
+
   stdout.write("Qual é a sua altura: ");
   double height = double.parse(stdin.readLineSync()!);
 
   stdout.write("Qual é o seu peso: ");
   double weight = double.parse(stdin.readLineSync()!);
 
-  double result = weight / (height * height);
+  var result = weight / (height * height);
   var imcFormat = result.toStringAsFixed(2);
 
   print(result);
 
-  if (result >= 40) {
-    print("Seu IMC está em $imcFormat/kg - Obesidade grau 3.");
-  } else if (result >= 35) {
-    print("Seu IMC está em $imcFormat/kg - Obesidade grau 2.");
-  } else if (result >= 30) {
-    print("Seu IMC está em $imcFormat/kg - Obesidade grau 1.");
-  } else if (result >= 25) {
-    print("Seu IMC está em $imcFormat/kg - Sobrepeso.");
-  } else if (result >= 18.5) {
-    print("Seu IMC está em $imcFormat/kg - Peso normal.");
+  if (result < 18.5) {
+    print("$name, seu IMC está em $imcFormat/kg - Abaixo do peso.");
+  } else if (result >= 18.5 && result <= 24.9) {
+    print("$name, seu IMC está em $imcFormat/kg - Peso normal.");
+  } else if (result >= 25 && result <= 29.9) {
+    print("$name, seu IMC está em $imcFormat/kg - Sobrepeso.");
+  } else if (result >= 30 && result <= 34.9) {
+    print("$name, seu IMC está em $imcFormat/kg - Obesidade grau 1.");
+  } else if (result >= 35 && result <= 39.9) {
+    print("$name, seu IMC está em $imcFormat/kg - Obesidade grau 2.");
   } else {
-    print("Seu IMC está em $imcFormat/kg - Abaixo do peso.");
+    print("$name, seu IMC está em $imcFormat/kg - Obesidade grau 3.");
   }
 }
