dc63872e69ae3a13e6130f09b06f2f2b3c47c332
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-01 21:38:38 -0300

    Exercicio idade

--- exercicio.dart
+++ exercicio.dart
@@ -1,16 +1,23 @@
-main() {
+import 'dart:io';
+
+void main() {
   // Verificar se uma pessoa é maior de idade (18)
 
   // Perguntar a idade da pessoa:
   // Se a idade for maior ou igual a 18, a pessoa é considerada maior de idade.
   // Se a idade for menor que 18, a pessoa é considerada menor de idade.
 
-  var age = 20;
+  print("Qual é a sua idade:");
 
-  if (age >= 18) {
-    print("Você é maior de idade");
-  } else {
-    print("Você é menor de idade");
+  String? ageInput = stdin.readLineSync();
+  if (ageInput != null) {
+    int age = int.parse(ageInput);
+
+    if (age >= 18) {
+      print("Você é maior de idade");
+    } else {
+      print("Você é menor de idade");
+    }
+
   }
-  
 }
