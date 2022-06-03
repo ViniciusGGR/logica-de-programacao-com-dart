c0860d77c3433a2b3180e519d9562238b6f5f8bc
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-03 16:53:11 -0300

    Acessando um item dentro do array

--- arrays_pt2.dart
+++ arrays_pt2.dart
@@ -2,10 +2,14 @@ main() {
   var listNames = ["Vinicius", "Sandra", "William", "Gabriel", "Matheus", "Pedro", "Guilherme"]; // Uma lista que pode receber qualquer tipo de dados - String, int, double...
   // Cada valor é separado por vírgula.
 
-  print(listNames);
-  print(listNames.length); // Retorna o tamanho/quantidade de dados dentro do array. No caso 7.
+  print("$listNames \n");
+  print("${listNames.length} \n"); // Retorna o tamanho/quantidade de dados dentro do array. No caso 7.
 
   listNames.add("Luiza"); // Adiciona mais um dado ao final do array.
 
-  print(listNames);
+  print("$listNames \n");
+
+  print("${listNames[2]} \n"); // Printa o dado que está na posição 2 do array.
+    // O dado "William".
+    // Dica: A posição no array começa com índice 0.
 }
