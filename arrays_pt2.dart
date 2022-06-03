2c9c1750dcbafb89775d22fc46c89fa4bdc26f6a
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-03 16:36:00 -0300

    Arrays pt.2

---
+++ arrays_pt2.dart
@@ -0,0 +1,11 @@
+main() {
+  var listNames = ["Vinicius", "Sandra", "William", "Gabriel", "Matheus", "Pedro", "Guilherme"]; // Uma lista que pode receber qualquer tipo de dados - String, int, double...
+  // Cada valor é separado por vírgula.
+
+  print(listNames);
+  print(listNames.length); // Retorna o tamanho/quantidade de dados dentro do array. No caso 7.
+
+  listNames.add("Luiza"); // Adiciona mais um dado ao final do array.
+
+  print(listNames);
+}
