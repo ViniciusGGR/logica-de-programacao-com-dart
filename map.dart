4e3c61bb2c621a21604636b9b7a643574b1458a0
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-06 18:17:21 -0300

    Introdução a maps

---
+++ map.dart
@@ -0,0 +1,17 @@
+main() {
+
+  var maps = { // Inicia-se um map como se fosse o escopo de uma função.
+    "name" : "Vinicius", // Primeiro valor é conhecido como key - Exemplo: "nome".
+    "age" : 27,
+    "city" : "Brasilia",
+    "country" : "Brasil"
+  }; // Um map se parece muito com um JSON - JS.
+
+  print(maps["name"]); // Acessando o dado dentro da key name - Retorna o valor "Vinicius".
+
+  print(maps); // Retorna todos os dados dentro do map em um formato de "JSON".
+
+  maps["name"] = "Gabriel"; // Atribuindo o valor "Gabriel" a key name dentro do map.
+  print(maps["name"]);
+
+}
