91cf8a214fd06e449427a54cc3735df18d424660
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-05 22:14:12 -0300

    Removendo item da lista

--- lista_de_compras.dart
+++ lista_de_compras.dart
@@ -6,7 +6,7 @@ main() {
   bool condition = true;
 
   while (condition) {
-    stdout.write("Adicionar item a lista de compras [Sair]: ");
+    stdout.write("Adicionar item a lista de compras [Sair|Remover]: ");
     String item = stdin.readLineSync()!;
 
     if (item == "Sair") {
@@ -14,6 +14,25 @@ main() {
 
       print("------ Programa finalizado ------ \n");
       condition = false;
+    } else if (item == "Remover") {
+
+      print("Qual item você deseja remover: ");
+
+      for (int i = 0; i < purchases.length; i++) { // purchases.length = quantidade de "itens" dentro do array purchases.
+        print("Item $i - ${purchases[i]}"); // Imprime os produtos em uma lista com cada produto com seu indice.
+      }
+
+      print("\n");
+
+      stdout.write("Digite o número do produto que deseja remover: ");
+      int remove = int.parse(stdin.readLineSync()!);
+
+      print("-------------------------------------");
+      
+      print("Produto ${purchases.removeAt(remove)} removido da lista");
+
+      print("-------------------------------------");
+
     } else {
       purchases.add(item);
       
@@ -27,7 +46,11 @@ main() {
 
   print("------------------------------------------------ \n");
   
+  print("Sua lista de compras é:"); 
+
   for (int i = 0; i < purchases.length; i++) { // purchases.length = quantidade de "itens" dentro do array purchases.
-    print("Item $i - ${purchases[i]}"); // Imprime os produtos em uma lista com cada produto com seu indice.
+
+    // Imprime os produtos em uma lista com cada produto com seu indice.
+    print("Item $i - ${purchases[i]}");
   }
 }
