d189934668dc0a54c69d23c95855cb4446f8c071
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-07 20:07:25 -0300

    Introdução a classes

--- poo/lib/main.dart
+++ poo/lib/main.dart
@@ -1,3 +1,11 @@
+import './src/person.dart';
+
 void main(List<String> arguments) {
   
+  // Acessando a classe Person do arquivo person.dart
+  Person person = Person(); // Person() - Classe/Objeto instanciado.
+  // Variável tipada do tipo Person.
+  
+  print(person.personalData()); // person. - Métodos, objetos e propriedades disponíveis para acessar na classe Person.
+
 }
---
+++ poo/lib/src/person.dart
@@ -0,0 +1,21 @@
+
+import 'dart:async';
+import 'dart:io';
+
+// Uma class representa um objeto em POO.
+class Person { // Nome da classe sempre deve começar com maiúscula.
+
+  personalData() { // Método para inserção dos dados pessoais.
+    
+    stdout.write("Qual é o seu nome: ");
+    String name = stdin.readLineSync()!; // Variável nome.
+
+    stdout.write("Qual a sua idade: ");
+    int age = int.parse(stdin.readLineSync()!); // Variável idade.
+  
+    stdout.write("Qual é o seu gênero: ");
+    String gender = stdin.readLineSync()!; // Variável gênero.
+  
+  }
+
+}
