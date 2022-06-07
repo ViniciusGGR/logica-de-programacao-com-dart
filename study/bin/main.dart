b3713bd63388ee5e76e5b3b04533d54c3a384c8e
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-07 12:28:00 -0300

    Importando arquivos

---
+++ study/bin/main.dart
@@ -0,0 +1,5 @@
+import '../lib/study.dart'; // Importando o arquivo study.dart da pasta lib.
+
+void main(List<String> arguments) {
+  print(calculate()); // Printando o método calculate do arquivo study que está na pasta lib.
+}
--- study/bin/study.dart
+++
@@ -1,5 +0,0 @@
-import 'package:study/study.dart' as study;
-
-void main(List<String> arguments) {
-  print('Hello world: ${study.calculate()}!');
-}
