9600df4e3b7c6f4d40e8b6e6e75850fcd0c909b9
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-07 12:48:04 -0300

    Introdução a argumentos

--- study/bin/main.dart
+++ study/bin/main.dart
@@ -1,5 +1,12 @@
 import '../lib/study.dart'; // Importando o arquivo study.dart da pasta lib.
 
-void main(List<String> arguments) {
+void main(List<String> arguments) { // Recebe uma lista de argumentos.
+
   print(calculate()); // Printando o método calculate do arquivo study que está na pasta lib.
+
+  print(arguments); // Retorna um array vazio.
+
+  // Executando esse arquivo = dart bin/main.dart.
+  // dart bin/main.dart Vinicius - Esse comando colocará o Vinicius dentro do array de argumentos.
+
 }
