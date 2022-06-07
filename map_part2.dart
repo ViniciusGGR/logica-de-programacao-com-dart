c6577b9ce42bfe5fcce552b704f98f8c7baa37d9
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-07 00:11:48 -0300

    Cadastrando user com while

--- map_part2.dart
+++ map_part2.dart
@@ -8,12 +8,41 @@ main() {
 
   // Cadastrando um usuário
 
+  bool condition = true;
+
+  while (condition) {
+
+    print("--------------------------------");
+    
+    stdout.write("Você deseja cadastrar um novo usuário [Sim|Nao]: ");
+    String text = stdin.readLineSync()!;
+
+    print("--------------------------------");
+
+    if (text == "Nao") {
+
+      print("Programa Finalizado");
+      condition = false;
+    
+    } else if (text == "Sim") {
+      
+      register();
+    
+    } else {
+      
+      print("Instrução invalida");
+
+    }
+  }
+
+}
+
+register() {
+  
   stdout.write("Qual é o seu nome: ");
   String nameInput = stdin.readLineSync()!;
 
   user["name"] = nameInput; // Criando a key name dentro do map user e inserindo o valor digitado em nameInput ao name dentro do Map
-
-  print(user);
 
   print("--------------------------------");
 
@@ -22,8 +51,6 @@ main() {
 
   user["age"] = ageInput; // Criando a key age dentro do map user e inserindo o valor digitado em ageInput ao age dentro do Map
 
-  print(user);
-
   print("--------------------------------");
 
   stdout.write("Qual é o seu país: ");
@@ -31,14 +58,14 @@ main() {
 
   user["country"] = countryInput; // Criando a key country dentro do map user e inserindo o valor digitado em countryInput ao country dentro do Map
 
-  print(user);
-
   print("--------------------------------");
 
   stdout.write("Qual é a sua cidade: ");
   String cityInput = stdin.readLineSync()!;
 
   user["city"] = cityInput; // Criando a key city dentro do map user e inserindo o valor digitado em cityInput ao city dentro do Map
+
+  print("--------------------------------");
 
   print(user);
 
