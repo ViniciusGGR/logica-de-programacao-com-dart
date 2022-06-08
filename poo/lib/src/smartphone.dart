3e7661b65e61e57921da3619d20998507d6b3c6b
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-08 15:04:54 -0300

    Protegendo variáveis

--- poo/lib/src/smartphone.dart
+++ poo/lib/src/smartphone.dart
@@ -2,13 +2,17 @@
 class Smartphone {
 
   // Objetos globais não precisam estar fora da classe.
-  final String? brand; // String? - Variável com valor nulo
-  final String? model; // String? - Variável com valor nulo
-  final String? color; // String? - Variável com valor nulo
+  String? brand; // String? - Variável com valor anulável
+  String? model; // String? - Variável com valor anulável
+  String? color; // String? - Variável com valor anulável
 
   // O construtor permite passar os valores na função enquanto está acontecendo o instanciamento da classe Smartphone dentro do arquivo main().
 
   // Para o construtor funcionar ele precisa ter o mesmo nome da classe na qual ele está.
   Smartphone({this.brand, this.model, this.color}); // This faz referência aos objetos globais da classe Smartphone - brand, model, color.
+
+  String _localName = "Vinícius"; // Variável local.
+  
+  final String lastName = "Gabriel"; // O final não permite uma nova atribuição de valor a essa variável.
 
 }
